9ba530de47decdb318637c1247cfa6adf49bd01c
younho <nnyyhh9247@gmail.com>
2021-11-17 11:26:22 +0900

    test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,15 @@
 import EventLog from 'page/eventLog/EventLog';
 import Main from 'page/main/Main';
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <>
-      <Switch>
-        <Route path="/main" component={Main} />
-        <Route path="/eventLog" component={EventLog} />
-      </Switch>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/eventLog" element={<EventLog />} />
+      </Routes>
     </>
   );
 }
