4548f9f07ec5b46f0ea5e4ededc86de9bb98b3b2
younho <nnyyhh9247@gmail.com>
2021-11-17 11:37:08 +0900

    added main page

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Main = () => {
-  return <div>Main 페이지 입니다.</div>;
+  return (
+    <>
+      <div>Main 페이지 입니다.</div>
+      <div>둘러보세요</div>
+    </>
+  );
 };
 
 export default Main;
